f18db885c02b7d1181b65ad225a17ca6492bc2f1
Brenton 'B-Train' Fletcher <i@bloople.net>
2010-05-27 09:12:54 +1000

    Fix typo

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 
-require 'net/http' #GOD DAMN YOU BUNDLER - requiring this here fixes wird require problem later on when bundle is locked. Hopefully this is the only one we need to do this for.
+require 'net/http' #GOD DAMN YOU BUNDLER - requiring this here fixes wierd require problem later on when bundle is locked. Hopefully this is the only one we need to do this for.
 
 # Set up gems listed in the Gemfile.
 if File.exist?(File.expand_path('../../Gemfile', __FILE__))
