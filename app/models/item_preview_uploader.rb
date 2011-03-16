521d683534569bc29465021cdf7c61be53bac123
Brenton 'B-Train' Fletcher <i@bloople.net>
2011-03-16 16:09:10 +1100

    Fix bad var name

--- app/models/item_preview_uploader.rb
+++ app/models/item_preview_uploader.rb
@@ -40,7 +40,7 @@ class ItemPreviewUploader < CarrierWave::Uploader::Base
   def handle_video_image(img, p_width, p_height)
     puts "p_width: #{p_width}, p_height: #{p_height}"
     if (img.rows > img.columns) && img.columns > p_width && img.rows > p_height #if it's portrait-oriented
-      img.crop!(Magick::CenterGravity, img.columns, img.coluchange_geometrymns / (p_width / p_height.to_f)) #Resize it so the right-most part of the image is shown
+      img.crop!(Magick::CenterGravity, img.columns, img.columns / (p_width / p_height.to_f)) #Resize it so the right-most part of the image is shown
     end
 
     img.change_geometry!("#{p_width}>") { |cols, rows, img| img.resize!(cols, rows) }
