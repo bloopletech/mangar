c4bf8f802d6f6e79363be98bbcb54949e9f4a8f8
Brenton 'B-Train' Fletcher <i@bloople.net>
2011-06-07 22:50:46 +1000

    Fix thumbnail

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,8 +4,8 @@ class Book < Item
   PREVIEW_WIDTH = 167
   PREVIEW_HEIGHT = 262
 
-  PREVIEW_SMALL_WIDTH = 100
-  PREVIEW_SMALL_HEIGHT = 157
+  PREVIEW_SMALL_WIDTH = 98
+  PREVIEW_SMALL_HEIGHT = 154
 
   #default_scope :order => 'published_on DESC'
 
