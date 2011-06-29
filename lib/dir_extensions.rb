fc9e71b2b385df52d514711ec6865c23d484fcbc
Brenton 'B-Train' Fletcher <i@bloople.net>
2011-06-30 00:27:38 +1000

    Fix regex

--- lib/dir_extensions.rb
+++ lib/dir_extensions.rb
@@ -2,6 +2,6 @@ class Dir
   #Note: will not return . and ..
   def self.deep_entries(dir)
     dir_regex = /^#{Regexp.escape(dir)}\//    
-    Dir.glob("#{dir}/**/*").map { |d| d.sub(dir_regex, '') }
+    Dir.glob("#{dir.gsub(/(\[|\]|\*|\?)/, "\\\\\\1")}/**/*").map { |d| d.sub(dir_regex, '') }
   end
 end
