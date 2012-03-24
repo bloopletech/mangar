0d877557dd04d41a6cf534d85158596b2c9f3caa
Brenton Fletcher <bloopletech@bloopletech.(none)>
2012-03-24 21:41:53 +1100

    Show page title everywhere

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,7 @@
 class VideosController < ItemsController
   def show
     @video = Video.find(params[:id])
+    @page_title = @video.title
 
     #["play", "gnome-mplayer", "mplayer -alang jpn -sid 0"].detect { |app| system("#{app} #{File.escape_name(@video.real_path)} &") }
   end
