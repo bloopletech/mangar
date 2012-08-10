d6866db2699694d4fc3cc577eac03835f7cac2da
Brenton 'B-Train' Fletcher <i@bloople.net>
2012-08-10 12:59:20 +1000

    Fix sorting (again)

--- lib/sort_extensions.rb
+++ lib/sort_extensions.rb
@@ -1,3 +1,16 @@
+=begin
+class String
+  alias_method :spaceship_original, :<=>
+  def <=>(s2)
+    if(!s2.is_a?(String))
+      self <=> s2.to_s
+    else
+      spaceship_original(s2)
+    end
+  end
+end
+=end
+
 class Fixnum
   alias_method :to_str, :to_s
 
@@ -7,6 +20,15 @@ class Fixnum
       self.to_s <=> s2
     else
       spaceship_original(s2)
+    end
+  end
+
+  alias_method :equals_original, :==
+  def ==(s2)
+    if(s2.is_a?(String))
+      return false
+    else
+      equals_original(s2)
     end
   end
 end
