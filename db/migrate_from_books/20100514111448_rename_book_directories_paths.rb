3f92137b1cde068e6d1e3a33668c9ebd3a907bc6
Brenton "B-Train" Fletcher <i@bloople.net>
2015-06-12 18:36:26 +1000

    Remove ancient unused migrations

--- db/migrate_from_books/20100514111448_rename_book_directories_paths.rb
+++
@@ -1,9 +0,0 @@
-class RenameBookDirectoriesPaths < ActiveRecord::Migration
-  def self.up
-    rename_column :books, :directory, :path
-  end
-
-  def self.down
-    rename_column :books, :path, :directory
-  end
-end
--- db/migrate_from_books/20101119115401_move_video_previews.rb
+++
@@ -1,9 +0,0 @@
-class MoveVideoPreviews < ActiveRecord::Migration
-  def self.up
-    FileUtils.mv("#{Mangar.mangar_dir}/public/system/videos", "#{Mangar.mangar_dir}/public/system/video_previews") if File.exists?("#{Mangar.mangar_dir}/public/system/videos")
-    FileUtils.mkdir("#{Mangar.mangar_dir}/public/system/videos") if !File.exists?("#{Mangar.mangar_dir}/public/system/videos")
-  end
-
-  def self.down
-  end
-end
